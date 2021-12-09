a0e8db694a1801254aa8b078f0440108642fe2c0
JesusIRL <82088522+JesusIRL@users.noreply.github.com>
2021-12-09 11:58:53 +0100

    Update Main.java
    String to Ascii

--- src/ch/kbw/ag/Main.java
+++ src/ch/kbw/ag/Main.java
@@ -3,6 +3,24 @@ package ch.kbw.ag;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String s = "Hello, there.";
+
+        System.out.println(AsciiToBinary(s));
+    }
+    public static String AsciiToBinary(String asciiString){
+
+        byte[] bytes = asciiString.getBytes();
+        StringBuilder binary = new StringBuilder();
+        for (byte b : bytes)
+        {
+            int val = b;
+            for (int i = 0; i < 8; i++)
+            {
+                binary.append((val & 128) == 0 ? 0 : 1);
+                val <<= 1;
+            }
+            binary.append(' ');
+        }
+        return binary.toString();
     }
 }
